5f9aaf8de2c39d57390a3df40762216a0228c423
Daniel Mesejo <mesejoleon@gmail.com>
2020-04-26 13:05:47 +0200

    change setup to configuration file

--- setup.py
+++ setup.py
@@ -1,8 +1,4 @@
 import setuptools
 
-setuptools.setup(
-    name="trex",
-    version="0.0.1",
-    description="Transform trie to regular expression",
-    packages=setuptools.find_packages(),
-)
+if __name__ == '__main__':
+    setuptools.setup()
