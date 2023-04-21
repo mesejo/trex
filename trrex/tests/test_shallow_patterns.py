e60edd3af459b21567ed14bcb2b29996e0cb01fb
Daniel Mesejo <mesejoleon@gmail.com>
2023-04-21 11:28:50 +0200

    tests based on regex

--- trrex/tests/test_shallow_patterns.py
+++ trrex/tests/test_shallow_patterns.py
@@ -1,64 +1,78 @@
 import re
 
+from hypothesis import given
+from hypothesis.strategies import from_regex
+
 from trrex import assemble
 
 
-def test_findall_repeat():
+@given(from_regex("ba{1,3}", fullmatch=True))
+def test_findall_repeat(string):
     words = ["bad", "bat"]
     patterns = ["ba{1,3}"]
     pattern = re.compile(assemble(words, patterns))
-    assert pattern.findall("The ba was bitten by the bad bat") == ["ba", "bad", "bat"]
-    assert pattern.findall("The baa was bitten by the bad bat") == ["baa", "bad", "bat"]
-    assert pattern.findall("The baaa was bitten by the bad bat") == [
-        "baaa",
+    assert pattern.findall(f"The {string} was bitten by the bad bat") == [
+        string,
         "bad",
         "bat",
     ]
 
 
-def test_findall_character_set():
+@given(from_regex("b[abc]", fullmatch=True))
+def test_findall_character_set(string):
     words = ["bad", "bat"]
     patterns = ["b[abc]"]
     pattern = re.compile(assemble(words, patterns))
-    assert pattern.findall("The ba was bitten by the bad bat") == ["ba", "bad", "bat"]
-    assert pattern.findall("The bb was bitten by the bad bat") == ["bb", "bad", "bat"]
-    assert pattern.findall("The bc was bitten by the bad bat") == ["bc", "bad", "bat"]
-
-
-def test_findall_range():
-    words = ["bad", "bat"]
-    patterns = ["b[d-f]"]
-    pattern = re.compile(assemble(words, patterns))
-    assert pattern.findall("The bd was bitten by the bad bat") == ["bd", "bad", "bat"]
-    assert pattern.findall("The be was bitten by the bad bat") == ["be", "bad", "bat"]
-    assert pattern.findall("The bf was bitten by the bad bat") == ["bf", "bad", "bat"]
-
-
-def test_findall_digit_category():
-    words = ["bad", "bat"]
-    patterns = [r"b\d"]
-    pattern = re.compile(assemble(words, patterns))
-    assert pattern.findall("The b1 was bitten by the bad bat") == ["b1", "bad", "bat"]
-
-
-def test_findall_multiple_prefixed_patterns():
-    words = ["bad", "bat"]
-    patterns = ["boy|baby"]
-    pattern = re.compile(assemble(words, patterns))
-    assert pattern.findall("The baby was bitten by the bad bat") == [
-        "baby",
+    assert pattern.findall(f"The {string} was bitten by the bad bat") == [
+        string,
         "bad",
         "bat",
     ]
-    assert pattern.findall("The boy was bitten by the bad bat") == ["boy", "bad", "bat"]
 
 
-def test_findall_word_category():
+@given(from_regex("b[d-f]", fullmatch=True))
+def test_findall_range(string):
+    words = ["bad", "bat"]
+    patterns = ["b[d-f]"]
+    pattern = re.compile(assemble(words, patterns))
+    assert pattern.findall(f"The {string} was bitten by the bad bat") == [
+        string,
+        "bad",
+        "bat",
+    ]
+
+
+@given(from_regex(r"b\d", fullmatch=True))
+def test_findall_digit_category(string):
+    words = ["bad", "bat"]
+    patterns = [r"b\d"]
+    pattern = re.compile(assemble(words, patterns))
+    assert pattern.findall(f"The {string} was bitten by the bad bat") == [
+        string,
+        "bad",
+        "bat",
+    ]
+
+
+@given(from_regex("boy|baby", fullmatch=True))
+def test_findall_multiple_prefixed_patterns(string):
+    words = ["bad", "bat"]
+    patterns = ["boy|baby"]
+    pattern = re.compile(assemble(words, patterns))
+    assert pattern.findall(f"The {string} was bitten by the bad bat") == [
+        string,
+        "bad",
+        "bat",
+    ]
+
+
+@given(from_regex(r"b\w", fullmatch=True))
+def test_findall_word_category(string):
     words = ["bad", "bat"]
     patterns = [r"b\w"]
     pattern = re.compile(assemble(words, patterns))
-    assert pattern.findall("The b1 was bitten by the bad bat") == [
-        "b1",
+    assert pattern.findall(f"The {string} was bitten by the bad bat") == [
+        string,
         "by",
         "bad",
         "bat",
