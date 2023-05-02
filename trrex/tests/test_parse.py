bf101c093e4450f58bee779e5ad64e4446da8286
Daniel Mesejo <mesejoleon@gmail.com>
2023-05-02 12:30:50 +0200

    improve tests by changing expected vs actual

--- trrex/tests/test_parse.py
+++ trrex/tests/test_parse.py
@@ -6,159 +6,159 @@ from trrex.parse import parse
 
 
 def test_basic_literal():
-    assert [["a"]] == parse("a")
+    assert parse("a") == [["a"]]
 
 
 def test_any():
-    assert [["."]] == parse(".")
+    assert parse(".") == [["."]]
 
 
 def test_at_beginning():
-    assert [["^", "a"]] == parse("^a")
+    assert parse("^a") == [["^", "a"]]
 
 
 def test_at_end():
-    assert [["a", "$"]] == parse("a$")
+    assert parse("a$") == [["a", "$"]]
 
 
 def test_max_repeat():
-    assert [["a", "b+", "c"]] == parse(r"ab+c")
+    assert parse(r"ab+c") == [["a", "b+", "c"]]
 
 
 def test_alternation_and_digit_category():
-    assert [["a", "b", r"\d"], ["b"]] == parse(r"ab\d|b")
+    assert parse(r"ab\d|b") == [["a", "b", r"\d"], ["b"]]
 
 
 def test_alternation_and_character_set():
-    assert [["a", "b", r"[c-z]"], ["b"]] == parse(r"ab[c-z]|b")
+    assert parse(r"ab[c-z]|b") == [["a", "b", r"[c-z]"], ["b"]]
 
 
 def test_complex_alternation():
-    assert [["a", "b", r"[c-z]", "a{1,3}"], ["b"]] == parse(r"ab[c-z]a{1,3}|b")
+    assert parse(r"ab[c-z]a{1,3}|b") == [["a", "b", r"[c-z]", "a{1,3}"], ["b"]]
 
 
 def test_simple_alternation():
-    assert [["a", "b"], ["b"]] == parse("ab|b")
+    assert parse("ab|b") == [["a", "b"], ["b"]]
 
 
 def test_literal_alternation():
-    assert [["a"], ["b"]] == parse("a|b")
+    assert parse("a|b") == [["a"], ["b"]]
 
 
 def test_single_char_inside_character_class():
-    assert [["a"]] == parse("[a]")
+    assert parse("[a]") == [["a"]]
 
 
 def test_hyphen_inside_character_class():
-    assert [["[a-]"]] == parse("[a-]")
+    assert parse("[a-]") == [["[a-]"]]
 
 
 def test_complex_pattern():
-    assert [["a{1,3}", "[b-z]", r"\d"]] == parse(r"a{1,3}[b-z]\d")
+    assert parse(r"a{1,3}[b-z]\d") == [["a{1,3}", "[b-z]", r"\d"]]
 
 
 def test_max_repeat_with_min():
-    assert [["a{1,3}", "b", "c", "d"]] == parse("a{1,3}bcd")
+    assert parse("a{1,3}bcd") == [["a{1,3}", "b", "c", "d"]]
 
 
 def test_digit_category_max_repeat():
-    assert [[r"\d{1,3}"]] == parse(r"\d{1,3}")
+    assert parse(r"\d{1,3}") == [[r"\d{1,3}"]]
 
 
 def test_two_char_literal():
-    assert [["a", "B"]] == parse("aB")
+    assert parse("aB") == [["a", "B"]]
 
 
 def test_complex_character_set():
-    assert [[r"[\dabc]"]] == parse(r"[\dabc]")
+    assert parse(r"[\dabc]") == [[r"[\dabc]"]]
 
 
 def test_character_set_with_hyphen():
-    assert [[r"[abc-]"]] == parse(r"[abc-]")
+    assert parse(r"[abc-]") == [[r"[abc-]"]]
 
 
 def test_category_digit():
-    assert [[r"\d"]] == parse(r"\d")
+    assert parse(r"\d") == [[r"\d"]]
 
 
 def test_range_in_character_set():
-    assert [["[a-e]"]] == parse("[a-e]")
+    assert parse("[a-e]") == [["[a-e]"]]
 
 
 def test_range_and_literal():
-    assert [["[a-e]", "x", "y", "z"]] == parse("[a-e]xyz")
+    assert parse("[a-e]xyz") == [["[a-e]", "x", "y", "z"]]
 
 
 def test_range_and_chars_in_character_set():
-    assert [["[a-exyz]"]] == parse("[a-exyz]")
+    assert parse("[a-exyz]") == [["[a-exyz]"]]
 
 
 def test_digit_category_in_character_set():
-    assert [[r"\d"]] == parse(r"[\d]")
+    assert parse(r"[\d]") == [[r"\d"]]
 
 
 def test_multiple_categories_in_character_set():
-    assert [[r"[\d\s]"]] == parse(r"[\d\s]")
+    assert parse(r"[\d\s]") == [[r"[\d\s]"]]
 
 
 def test_different_encodings_in_character_set():
-    assert [[r"[Ça(]"]] == parse(r"[\N{LATIN CAPITAL LETTER C WITH CEDILLA}\x61\050]")
+    assert parse(r"[\N{LATIN CAPITAL LETTER C WITH CEDILLA}\x61\050]") == [[r"[Ça(]"]]
 
 
 def test_range_and_category_digit():
-    assert [[r"[a-e\d]"]] == parse(r"[a-e\d]")
+    assert parse(r"[a-e\d]") == [[r"[a-e\d]"]]
 
 
 def test_escape_in_character_class():
-    assert [[r"[\t\a]"]] == parse(r"[\t\a]")
+    assert parse(r"[\t\a]") == [[r"[\t\a]"]]
 
 
 def test_simple_max_repeat():
-    assert [["a{1,3}"]] == parse("a{1,3}")
+    assert parse("a{1,3}") == [["a{1,3}"]]
 
 
 def test_empty_repeat():
-    assert [["a", "{", "}"]] == parse("a{}")
+    assert parse("a{}") == [["a", "{", "}"]]
 
 
 def test_half_repeat():
-    assert [["a", "{"]] == parse("a{")
+    assert parse("a{") == [["a", "{"]]
 
 
 def test_not_digit_category():
-    assert [[r"\D"]] == parse(r"\D")
+    assert parse(r"\D") == [[r"\D"]]
 
 
 def test_space_category():
-    assert [[r"\s"]] == parse(r"\s")
+    assert parse(r"\s") == [[r"\s"]]
 
 
 def test_not_space_category():
-    assert [[r"\S"]] == parse(r"\S")
+    assert parse(r"\S") == [[r"\S"]]
 
 
 def test_word_category():
-    assert [[r"\w"]] == parse(r"\w")
+    assert parse(r"\w") == [[r"\w"]]
 
 
 def test_not_word_category():
-    assert [[r"\W"]] == parse(r"\W")
+    assert parse(r"\W") == [[r"\W"]]
 
 
 def test_boundary_category():
-    assert [[r"\b"]] == parse(r"\b")
+    assert parse(r"\b") == [[r"\b"]]
 
 
 def test_not_boundary_category():
-    assert [[r"\B"]] == parse(r"\B")
+    assert parse(r"\B") == [[r"\B"]]
 
 
 def test_at_beginning_category():
-    assert [[r"\A"]] == parse(r"\A")
+    assert parse(r"\A") == [[r"\A"]]
 
 
 def test_repeat_at_most_one():
-    assert [["[a-z]{0,1}"]] == parse(r"[a-z]?")
+    assert parse(r"[a-z]?") == [["[a-z]{0,1}"]]
 
 
 def test_min_repeat():
@@ -178,28 +178,64 @@ def test_min_repeat_zero_or_more():
 
 
 def test_named_unicode():
-    assert [[r"Ç"]] == parse(r"\N{LATIN CAPITAL LETTER C WITH CEDILLA}")
+    assert parse(r"\N{LATIN CAPITAL LETTER C WITH CEDILLA}") == [[r"Ç"]]
+
+
+def test_unicode_character_set_escape():
+    assert parse(r"[\u0061-\u007a]+") == [["[a-z]+"]]
+
+
+def test_single_unicode():
+    assert parse(r"\u0061") == [["a"]]
+
+
+def test_unicode_character_set_escape_long():
+    assert parse(r"[\U00000061-\U0000007a]+") == [["[a-z]+"]]
+
+
+def test_single_unicode_long():
+    assert parse(r"\U00000061") == [["a"]]
+
+
+def test_hex_numbers():
+    assert parse(r"\x61") == [["a"]]
+
+
+def test_oct_numbers():
+    assert parse(r"\050") == [["("]]
+
+
+def test_at_end_category():
+    assert parse(r"\Z") == [[r"\Z"]]
+
+
+def test_escaped_parenthesis():
+    assert parse(r"\(") == [[r"\("]]
+
+
+def test_escaped_category_digit():
+    assert parse(re.escape(r"\d")) == [["\\\\", "d"]]
+
+
+def test_escaped_parenthesis_and_pattern():
+    assert parse(r"\(abc") == [["\\(", "a", "b", "c"]]
+
+
+def test_escaped_optional():
+    assert parse(re.escape("?abc")) == [["\\?", "a", "b", "c"]]
+
+
+def test_no_nested_not_capturing_patterns_escape():
+    assert parse(re.escape("(?:abc)")) == [["\\(", "\\?", ":", "a", "b", "c", "\\)"]]
+
+
+def test_character_set_with_meta_characters():
+    assert parse("[(?:]") == [["[(?:]"]]
 
 
 def test_named_unicode_missing_name():
     with pytest.raises(Exception):
         parse(r"\N{foobarbruxu}")
-
-
-def test_unicode_character_set_escape():
-    assert [["[a-z]+"]] == parse(r"[\u0061-\u007a]+")
-
-
-def test_single_unicode():
-    assert [["a"]] == parse(r"\u0061")
-
-
-def test_unicode_character_set_escape_long():
-    assert [["[a-z]+"]] == parse(r"[\U00000061-\U0000007a]+")
-
-
-def test_single_unicode_long():
-    assert [["a"]] == parse(r"\U00000061")
 
 
 def test_unicode_error():
@@ -212,18 +248,6 @@ def test_unicode_error_long():
         parse(r"\U05+")
 
 
-def test_hex_numbers():
-    assert [["a"]] == parse(r"\x61")
-
-
-def test_oct_numbers():
-    assert [["("]] == parse(r"\050")
-
-
-def test_at_end_category():
-    assert [[r"\Z"]] == parse(r"\Z")
-
-
 def test_bad_escape():
     with pytest.raises(Exception):
         parse(r"\C")
@@ -232,22 +256,6 @@ def test_bad_escape():
 def test_bad_escape_in_character_set():
     with pytest.raises(Exception):
         parse(r"[\C]")
-
-
-def test_escaped_parenthesis():
-    assert [[r"\("]] == parse(r"\(")
-
-
-def test_escaped_category_digit():
-    assert [["\\\\", "d"]] == parse(re.escape(r"\d"))
-
-
-def test_escaped_parenthesis_and_pattern():
-    assert [["\\(", "a", "b", "c"]] == parse(r"\(abc")
-
-
-def test_escaped_optional():
-    assert [["\\?", "a", "b", "c"]] == parse(re.escape("?abc"))
 
 
 def test_not_escaped_parenthesis():
@@ -268,14 +276,6 @@ def test_no_nested_patterns():
 def test_not_capturing_patterns():
     with pytest.raises(NotImplementedError):
         parse("(?:abc)?")
-
-
-def test_no_nested_not_capturing_patterns_escape():
-    assert [["\\(", "\\?", ":", "a", "b", "c", "\\)"]] == parse(re.escape("(?:abc)"))
-
-
-def test_character_set_with_meta_characters():
-    assert [["[(?:]"]] == parse("[(?:]")
 
 
 def test_no_nested_patterns_assert_not():
